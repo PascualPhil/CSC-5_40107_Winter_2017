f54859c5dd490f9746f55c1698de413b342dc5c4
PascualPhil <ppascual1@student.rcc.edu>
2017-01-05 13:59:59 -0800

    Update main.cpp

--- CSC-5 Template (COPY ONLY)/main.cpp
+++ CSC-5 Template (COPY ONLY)/main.cpp
@@ -1,25 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
- * File:   main.cpp
- * Author: rcc
- *
- * Created on January 5, 2017, 1:39 PM
+  File:   main.cpp
+  Author: Phillip Pascual
+  Created on January 3, 2017, 12:15 PM
+  Purpose:  Template to be used in all programming
+            projects!
  */
 
-#include <cstdlib>
-
+//System Libraries
+#include <iostream>
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+//User Libraries
 
+//Global Constants
+//Such as PI, Vc, -> Math/Science values
+//as well as conversions from system of units to 
+//another
+
+//Function Prototypes
+
+//Executable code begins here!!!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input values
+    
+    //Process by mapping inputs to outputs
+    
+    //Output values
+
+    //Exit stage right!
     return 0;
 }
-
